9a16d83c071e5d22cb3d199e8b1bc65e7da60b29
Jos Ahrens <buughost@gmail.com>
2016-09-29 10:29:45 +0000

    notaru.rb plugin: support setting nick parameter in retainnick

--- lib/notaru/plugins/notaru.rb
+++ lib/notaru/plugins/notaru.rb
@@ -3,29 +3,35 @@ module Notaru
     class Notaru
       include Cinch::Plugin
 
-      match Regexp.new('notaru ([^ ]+)'), method: :cmd_notaru
-      def cmd_notaru(m, command)
+      match Regexp.new('notaru ([^ ]+)(?: (.+))?'), method: :cmd_notaru
+      def cmd_notaru(m, command, params)
         if m.channel.opped?(m.user)
           if command == 'help'
-            m.user.notice('Usage: notaru [help | docs | retainnick]')
+            m.user.notice('Usage: notaru [help | docs | retainnick [newnick]]')
           elsif command == 'docs'
             m.reply('Notaru extended docs: https://github.com/Zarthus/lobby-utility-bot/wiki')
           elsif command == 'retainnick'
-            m.reply(retainnick)
+            m.reply(retainnick(params))
           end
         end
       end
 
+      # @param [String] params
       # @return [String]
-      def retainnick
-        newnick = @bot.config.nick
-
-        if newnick.nil?
-          newnick = @bot.config.nicks.first
+      def retainnick(params = nil)
+        if params.nil?
+          newnick = @bot.config.nick
+ 
+          if newnick.nil?
+            newnick = @bot.config.nicks.first
+          end
+        else
+          return "Illegal parameters supplied." if /[ .@#!]/.match(params)
+          newnick = params
         end
-
+        
         if @bot.nick == newnick
-          return 'My name is already correct.'
+          return "My name is already correct. (#{@bot.nick} == #{newnick})"
         end
 
         @bot.nick = newnick
