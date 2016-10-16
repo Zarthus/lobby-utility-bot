7141cc29231cde6fb0262eaa14b90e1fc6a8cc97
Jos Ahrens <buughost@gmail.com>
2016-10-16 14:04:49 +0000

    quotes: better searching, add quotes command

--- lib/notaru/plugins/quotes.rb
+++ lib/notaru/plugins/quotes.rb
@@ -1,6 +1,7 @@
 require 'time'
 require 'cinch'
 require 'yaml'
+require 'cgi'
 
 module Notaru
   module Plugin
@@ -8,7 +9,8 @@ module Notaru
       include Cinch::Plugin
 
       match Regexp.new('(?:addquote|quoteadd) (.+)', Regexp::IGNORECASE), method: :addquote
-      match Regexp.new('quote(?: (.+))?', Regexp::IGNORECASE), method: :quote
+      match Regexp.new('quote(?: (.+))?\s*$', Regexp::IGNORECASE), method: :quote
+      match Regexp.new('quotes\s*$', Regexp::IGNORECASE), method: :quotecount
       match Regexp.new('(?:delquote|quotedel|undelquote|quoteundel) (\d+)', Regexp::IGNORECASE), method: :delquote
 
       def initialize(*args)
@@ -48,15 +50,25 @@ module Notaru
       end
 
       def delquote(m, quote_id)
-        if m.channel && m.channel.opped?(m.user)
+        if !m.channel || !m.channel.opped?(m.user)
           return m.reply('You need to be a channel op to remove or restore quotes.')
         end
 
         existing_quotes = retrieve_quotes || []
-        target_quote = existing_quotes.delete_if { |q| q['id'] != quote_id || q['channel'] != m.channel.name }
+        target_quote = existing_quotes.clone.delete_if { |q| q['id'] != quote_id.to_i }
 
         if target_quote.empty?
           return m.reply('No quote with that ID found.')
+        end
+
+        if target_quote.size > 1
+          return m.reply('Somehow, more than one result was found. @_@')
+        end
+
+        target_quote = target_quote.first
+
+        if target_quote['channel'] != m.channel.name
+          return m.reply('Channel mismatch, you must delete quotes in the channel they were added in.')
         end
 
         target_quote['deleted'] = !target_quote['deleted']
@@ -67,12 +79,19 @@ module Notaru
         end
 
         existing_quotes[idx] = target_quote
-
         output = File.new(@quotes_file, 'w')
         output.puts YAML.dump(existing_quotes)
         output.close
 
-        m.reply('Successfully ' + (target_quote['deleted'] ? 'deleted' : 'restored') + ' quote ID ' + target_quote['id'])
+        m.reply('Successfully ' + (target_quote['deleted'] ? 'deleted' : 'restored') + ' quote ID ' + target_quote['id'].to_s)
+      end
+
+      def quotecount(m)
+        qret = retrieve_quotes()
+        del_count = qret.reject { |q| !q['deleted'] }.size
+        chan_count = qret.reject { |q| q['channel'] != m.channel.name }.size
+
+        m.reply("Quotes: #{qret.size()} quotes are found, of which #{del_count} are deleted, and #{chan_count} from this channel.")
       end
 
       def quote(m, search = nil)
@@ -80,7 +99,7 @@ module Notaru
         if search.nil? # we are pulling random
           quote = quotes.sample
           m.reply "#{m.user.nick}: \\#{quote['id']} - #{fmt_quote(quote['quote'])}"
-        elsif search.to_i != 0 # then we are searching by id
+        elsif /^\d+$/.match(search) && search.to_i != 0 # then we are searching by id
           quote = quotes.find { |q| q['id'] == search.to_i }
           if quote.nil?
             m.reply "#{m.user.nick}: No quotes found."
@@ -94,9 +113,12 @@ module Notaru
           else
             quote = quotes.first
             m.reply "#{m.user.nick}: \\#{quote['id']} - #{fmt_quote(quote['quote'])}"
-            m.reply "The search term also matched on quote IDs: #{quotes.map { |q| q['id'] }.join(', ')}" if quotes.size > 1
-            if quotes.size > 3 && @quotes_url
-                m.reply "You can view them all here: " + @quotes_url + "?quotes=" + quotes.map { |q| q['id'] }.join(',')
+            if quotes.size > 20 && @quotes_url
+              m.reply "Too many results (#{quotes.size}) found while searching for more, view them here: " + @quotes_url + "?search=" + CGI.escape(search)
+            elsif quotes.size > 3 && @quotes_url
+              m.reply "#{quotes.size - 1} more results, view them here: " + @quotes_url + "?quotes=" + quotes.map { |q| q['id'] }.join(',')
+            elsif quotes.size > 1
+              m.reply "The search term also matched on quote IDs: " + quotes.map { |q| q['id'] }.join(', ').sub(quote['id'].to_s + ', ', '')
             end
           end
         end
